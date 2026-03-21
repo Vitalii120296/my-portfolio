c3d2c05839df6e604614a919f650cde4d65a8a1f
Vitalii <v.hulaievych@gmail.com>
2026-03-21 18:55:23 +0000

    fixed tooltip

--- app/components/ui/Tooltip.tsx
+++ app/components/ui/Tooltip.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const Tooltip: React.FC<Props> = ({ value }) => {
   return (
-    <div className="absolute px-3 py-2 mb-2 text-xs text-foreground transition-all duration-300 scale-0 -translate-x-1/2 bg-black rounded-md opacity-0 bottom-full left-1/2 whitespace-nowrap">
+    <div className="absolute px-3 py-2 mb-2 text-xs transition-all duration-300 scale-0 -translate-x-1/2 bg-black rounded-md opacity-0 text-foreground group-hover:opacity-100 group-hover:scale-100 bottom-full left-1/2 whitespace-nowrap">
       {value}
       <div className="absolute w-2 h-2 rotate-45 -translate-x-1/2 bg-black left-1/2 -bottom-1"></div>
     </div>
